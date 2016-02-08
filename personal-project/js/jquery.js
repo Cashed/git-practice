bff61d3bd52364a064becf578e10ef104a1cc393
Cashed <frau.veronika@gmail.com>
2016-02-07 23:12:55 -0800

    add kenshin jquery, finish future page

--- personal-project/js/jquery.js
+++ personal-project/js/jquery.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
 	$(".contact-button").mouseenter(function(){
 		$(".jake").show();
+		$(".kenshin").show();
 	})
 	.mouseleave(function(){
 		$(".jake").hide();
+		$(".kenshin").hide();
 	})
 });
